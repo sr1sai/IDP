94f28fc149472c944235a523b8393d1e5dc511ee
unknown <srisaidivyakola@gmail.com>
2023-10-30 23:54:03 +0530

    301023:2354

--- finalcode/final.py
+++ finalcode/final.py
@@ -5,9 +5,10 @@ import time
 import torch
 import torchvision.transforms as transforms
 from PIL import Image
+import subprocess
 
 def extractor(vid_path):
-    
+    frame_count=1
     cap = cv2.VideoCapture(vid_path)
 
     # Check if the video file was opened successfully
@@ -18,18 +19,18 @@ def extractor(vid_path):
     # Initialize a list to store unique frames
     frames = []
 
-    # Initialize a variable to keep track of frame numbers
-    frame_count = 0
 
     # Read the first frame
     ret, prev_frame = cap.read()
-
-    # Convert the frame to a grayscale image for faster comparison
-    #prev_frame_gray = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
-
-    frames.append(prev_frame)
-
+    print("Frame:",frame_count)
+    if ret:
+        # Convert the frame to a grayscale image for faster comparison
+        #prev_frame_gray = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
+        frames.append(prev_frame)
+        print("\tInserted")
+    frame_count+=1
     while True:
+        print("Frame:",frame_count)
         ret, frame = cap.read()
 
         # Break the loop when we reach the end of the video
@@ -46,19 +47,21 @@ def extractor(vid_path):
         # add it to the list of frames and update the previous frame
         if np.mean(frame_diff) > 5:
             frames.append(frame)
+            print("\tInserted")
         #  prev_frame_gray = frame_gray.copy()
             prev_frame = frame.copy()
-            
+        else:
+            print("\tRejected")
+        frame_count+=1
         # Display the frame (optional)
         # cv2.imshow('Frame', frame_gray)
         # Press 'q' to exit the loop
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        #if cv2.waitKey(1) & 0xFF == ord('q'):
+        #    break
 
         # Release the video capture object and close any open windows
     cap.release()
     cv2.destroyAllWindows()
-    print("no of frames: ",len(frames))
 
     video_name = os.path.basename(vid_path).split('.')[0]
 
@@ -73,43 +76,53 @@ def extractor(vid_path):
 
 
 def check_image_high_resolution(image):
+    if isinstance(image, np.ndarray):
+        return False  # Frames are numpy arrays, so high resolution check is not applicable for video frames
     width, height = image.size
-    threshold_width = 2048  # Define the threshold width for high resolution
-    threshold_height = 2048  # Define the threshold height for high resolution
+    threshold_width = 2048
+    threshold_height = 2048
     if width >= threshold_width or height >= threshold_height:
         print("Image is already at a high resolution, skipping upscaling.")
         return True
     return False
 
 def enhance_photos(photo_dict):
-    enhanced_photos = {}
-    model_path = 'C:\Users\Sri Sai\OneDrive\Desktop\IDP\ESRGAN\models\RRDB_ESRGAN_x4.pth'
-    model = torch.load(model_path)
-
+    lr_folder = 'C:\\Users\\Sri Sai\\OneDrive\\Desktop\\IDP\\ESRGAN\\LR'
     for photo_name, photo in photo_dict.items():
         input_image = photo
-
+        print(photo_name,"loaded",end=" ---> ")
         if check_image_high_resolution(input_image):
-            enhanced_photos[photo_name] = input_image
-            continue  # Skip processing if the image is already at a high resolution
+            print("uploaded")
+            # If it's a valid image, save it to the LR folder
+            photo_path = os.path.join(lr_folder, f'{photo_name}.jpg')
+            cv2.imwrite(photo_path, photo)
+        else:
+            print("Failed")
 
-        preprocess = transforms.Compose([
-            transforms.ToTensor()    # Convert to tensor
-        ])
-        input_tensor = preprocess(input_image).unsqueeze(0)
+    print("Sub Process execution started")
+    time.sleep(1)
+    # After saving the images, run the command in the terminal
+    process = subprocess.Popen(["python", "C:\\Users\\Sri Sai\\OneDrive\\Desktop\\IDP\\ESRGAN\\test.py"])
+    process.wait()  # This will wait until the subprocess finishes
+    if process.returncode == 0:
+        print("Command executed successfully.")
+    else:
+        print("Command execution failed.")
+    print("------'Phase_2 : The Enhancer' completed -------")
+    
 
-        with torch.no_grad():
-            output_tensor = model(input_tensor)
+vid_path="./finalcode/vid5.mp4"
+print("------'Phase_1 : The Extraction' commenced -------")
+time.sleep(1)
 
-        output_image = transforms.ToPILImage()(output_tensor.squeeze(0))
-        enhanced_photos[photo_name] = output_image
+ext_start=time.time()
 
-    return enhanced_photos
-
-vid_path="./finalcode/vid3.mp4"
+ext_func_start=time.time()
 frames=extractor(vid_path)
-print(frames.keys())
+ext_func_end=time.time()
 
+print("------Extraction completed -------")
+time.sleep(1)
 
 #checking the frames
 """for frame_name, frame in frames.items():
@@ -120,23 +133,49 @@ print(frames.keys())
 cv2.waitKey(0)  # Wait for any key to be pressed to close all frame windows
 cv2.destroyAllWindows()"""
 
+print("------Creating output Foldef -------")
+time.sleep(1)
 
+ext_folder_creating_start=time.time()
 output_folder = './finalcode/input'
 if not os.path.exists(output_folder):
   os.makedirs(output_folder)
+ext_folder_creating_end=time.time()
 
+print("------ Output Folder Created -------")
+time.sleep(1)
 # Save the frames to the "input" folder
+print("------ Commensing File Upload -------")
+time.sleep(1)
 
+ext_file_save_start=time.time()
 video_name = os.path.basename(vid_path).split('.')[0]
-
 for i, frame in frames.items():
-  frame_filename = os.path.join(output_folder, f'{i}.jpg')
-  cv2.imwrite(frame_filename, frame)
-  
+    print("Saving",i)
+    frame_filename = os.path.join(output_folder, f'{i}.jpg')
+    cv2.imwrite(frame_filename, frame)
+ext_file_save_end=time.time()
 
-frames=enhance_photos(frames)
-count=0
-for i, frame in frames.items():
+print("------'Phase_1 : The Extraction' completed -------")
+time.sleep(1)
+ext_end=time.time()
+print("------'Phase_2 : The Enhancer' commenced -------")
+time.sleep(1)
+
+enh_func_start=time.time()
+enhanced_frames=enhance_photos(frames)
+enh_func_end=time.time()
+
+"""count=0
+for i, frame in enhanced_frames.items():
   frame_filename = os.path.join(output_folder, f'{video_name}_frame_00{count}.jpg')
   cv2.imwrite(frame_filename, frame)
-  count+=1
+  count+=1"""
+
+
+#statistics
+print("Extraction Total:",ext_end-ext_start-5)
+print("\tExtraction Function:",ext_func_end-ext_func_start)
+print("\tExtraction Folder Creation:",ext_folder_creating_end-ext_folder_creating_start)
+print("\tExtraction File Uploading:",ext_file_save_end-ext_file_save_start)
+print("Enhancer Total:",enh_func_end-enh_func_start)
